a66249d18ed7c027621fb3e8a13ee80b47373bdb
Ayush <tripathyayush123@gmail.com>
2021-11-30 13:40:52 +0530

    added-searchFilter

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,7 +1,111 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { MdCancel } from "react-icons/md";
+import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
+import { baseUrl, fetchAPI } from "../utils/fetchAPI";
+import noresult from "../assets/images/noresult.svg";
 
 const SearchFilters = () => {
-  return <div>Filter</div>;
+  const [filters, setFilters] = useState(filterData);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [locationData, setLocationData] = useState();
+  const [showLocations, setShowLocations] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues);
+
+    values.forEach((item) => {
+      query[item.name] = item.value;
+    });
+
+    router.push({ pathname: path, query });
+  };
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      const fetchData = async () => {
+        setLoading(true);
+        const data = await fetchAPI(
+          `${baseUrl}/auto-complete?query=${searchTerm}`
+        );
+        setLoading(false);
+        setLocationData(data?.hits);
+      };
+
+      fetchData();
+    }
+  }, [searchTerm]);
+
+  return (
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            width="fit-content"
+            p="2"
+            onChange={(event) =>
+              searchProperties({ [filter.queryName]: event.target.value })
+            }
+          >
+            {filter?.items.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+      <Flex flexDir="column">
+        <Button
+          onClick={() => setShowLocations(!showLocations)}
+          border="1px"
+          borderColor="gray.200"
+          marginTop="2"
+        >
+          Search Location
+        </Button>
+        {showLocations && (
+          <Flex flexDir="column" pos="relative" paddingTop="2">
+            <Input
+              placeholder="Type Here"
+              value={searchTerm}
+              w="300px"
+              focusBorderColor="gray.300"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            {searchTerm !== "" && (
+              <Icon
+                as={MdCancel}
+                pos="absolute"
+                cursor="pointer"
+                right="5"
+                top="5"
+                zIndex="100"
+                onClick={() => setSearchTerm("")}
+              />
+            )}
+          </Flex>
+        )}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default SearchFilters;
